e1bcbd9b0c14206fe4ba6aa37eac8130f60ce0c4
hammed Akinwale <hammedakinwale.com>
2024-01-15 13:32:50 +0100

    adding API id and fetching docker images

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,6 +1,6 @@
 def call(String dockerHubUsername, String imageName) {
     // Build the Docker image
-    sh "docker build --build-arg REACT_APP_RAPID_API_KEY=f8f7ba69f4mshf12bf61c0f3cc65p10ec63jsna5efd664503d -t ${imageName} ."
+    sh "docker build --build-arg REACT_APP_RAPID_API_KEY= 'f8f7ba69f4mshf12bf61c0f3cc65p10ec63jsna5efd664503d' -t ${imageName} ."
      // Tag the Docker image
     sh "docker tag ${imageName} ${dockerHubUsername}/${imageName}:latest"
     // Push the Docker image
